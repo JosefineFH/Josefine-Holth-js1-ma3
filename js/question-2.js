176315f68e0d5ff795e03c0ba98ae32d4e7a3f4b
Josefine Holth <Josefine.Holth@hotmail.com>
2021-08-25 18:51:18 +0200

    almost finished with the code
    - do more with the HTML
    - set a timer on the code so the spinner can run longer so you can se that somthing is happening.

---
+++ js/question-2.js
@@ -0,0 +1,35 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=";
+const apiKey = "6081baf61dda43819e5c54c649a978b2";
+
+const urlWithAPIKey = url + apiKey;
+
+const divContainer = document.querySelector("section");
+
+fetch(urlWithAPIKey)
+    .then(respone => respone.json())
+    .then(data => {
+        
+        //sett timer for the divContainer.innerHTML = "";
+        divContainer.innerHTML = "";
+
+        const result = data.results;
+
+
+        for (let i = 0; i < result.length; i++) {
+            let gameName = result[i].name;
+            let rating = result[i].rating;
+            let numberOfTags = result[i].tags.length;
+
+            divContainer.innerHTML += ` 
+            <p>${gameName}</p>
+            <p>${rating}</p>
+            <p>${numberOfTags}</p>`;
+
+            if (i === 7) {
+                break;
+            }
+        }
+    })
+    .catch(error => {
+        divContainer.innerHTML = alertMessage("error", error);
+    })
